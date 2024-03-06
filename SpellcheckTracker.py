7ff5589eef2c25ddc2b6994f52f3703d13ba45a8
CCShoop <caelantennis@gmail.com>
2024-03-06 02:40:51 -0500

    allowing image attachment to results message

--- SpellcheckTracker.py
+++ SpellcheckTracker.py
@@ -258,7 +258,7 @@ def main():
 
             # process player's results
             await client.process(message, player)
-        elif message.channel.id == client.text_channel.id and message.attachments and message.attachments[0].is_spoiler():
+        if message.channel.id == client.text_channel.id and message.attachments and message.attachments[0].is_spoiler():
             for player in client.players:
                 if message.author.name == player.name:
                     if player.filePath == '':
